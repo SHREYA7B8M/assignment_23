d48e8ee98c74fcb34e3b6155b1f19aa69e244f31
s66829020@gmail.com <s66829020@gmail.com>
2023-08-04 15:43:43 +0530

    first commit

---
+++ MainApp_assignment23/MainApp_assignment23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary_assignment23;
+
+namespace MainApp_assignment23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userManagement = new UserManagement();
+            userManagement.RegisterUser("example@example.com", "securepassword");
+            Console.ReadKey();
+        }
+    }
+}
